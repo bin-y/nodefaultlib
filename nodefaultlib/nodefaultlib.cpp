6fbec222978a4d798990c0a153b6a74d59f917b7
bin-y <wye.mia.kid.c@gmail.com>
2015-02-07 21:24:47 +0800

    correction on casting

--- nodefaultlib/nodefaultlib.cpp
+++ nodefaultlib/nodefaultlib.cpp
@@ -100,7 +100,7 @@ int _tmain(int argc, _TCHAR* argv[])
 		BYTE* pbMemberContent = pbFileData + ullOffset + sizeof(IMAGE_ARCHIVE_MEMBER_HEADER);
 
 		//Skip short import library member
-		IMPORT_OBJECT_HEADER * pImportHeader = (ANON_OBJECT_HEADER*)pbMemberContent;
+		IMPORT_OBJECT_HEADER * pImportHeader = (IMPORT_OBJECT_HEADER*)pbMemberContent;
 		if (pImportHeader->Sig1 == IMAGE_FILE_MACHINE_UNKNOWN
 			&& pImportHeader->Sig2 == IMPORT_OBJECT_HDR_SIG2)
 		{
